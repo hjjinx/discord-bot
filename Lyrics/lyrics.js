b0fb6bcf9b23ff534792600ff9eb066d5a3e961c
Himanshu Jain <hjmanojain.8@gmail.com>
2020-04-25 14:40:43 +0530

    Remove MusixMatch API code. Change react mechanism upon YouTube results

--- Lyrics/lyrics.js
+++ Lyrics/lyrics.js
@@ -1,89 +1,11 @@
-const Discord = require("discord.js");
-const axios = require("axios");
-const token = require("../secret.json").musixmatch_token;
-const root = "https://api.musixmatch.com/ws/1.1";
 const gLyrics = require("./googleLyricsApi");
 
-module.exports.getLyrics = async message => {
+module.exports.getLyrics = async (message) => {
   if (message.content.includes(`/lyrics`))
     message.content = message.content.substr(8);
-  let arrTracks = [];
-  let res = await axios.get(
-    `${root}/track.search?apikey=${token}&q_track_artist=${message.content}&s_track_rating=desc&page_size=5`
-  );
   const lyrics = await gLyrics(message.content);
 
   sendLyrics(message, lyrics);
-  // const tracks = res.data.message.body.track_list;
-  // tracks.forEach(track =>
-  //   arrTracks.push({
-  //     ID: `${track.track.commontrack_id}`,
-  //     track: `${track.track.track_name}`,
-  //     artist: `${track.track.artist_name}`
-  //   })
-  // );
-  // if (arrTracks.length == 0) {
-  //   message.channel.send(
-  //     `No Lyrics found for the provided search query in the Musixmatch Database. Please be specific while searching and only write the name of the song and the artist.`
-  //   );
-  //   return;
-  // }
-  // const embed = new Discord.MessageEmbed()
-  //   .setTitle("Lyrics Found:")
-  //   .setColor(0xff00aa)
-  //   .setDescription(
-  //     "Which lyrics are you looking for? React with the #️⃣ of the track listed below within 10 seconds:"
-  //   );
-  // for (let i = 0; i < arrTracks.length; i++) {
-  //   embed.addField(`${i + 1}. ${arrTracks[i].track}`, arrTracks[i].artist);
-  // }
-
-  // message.channel
-  //   .send({ embed })
-  //   .then(async msg => {
-  //     const reactions = [
-  //       "\u0031\u20E3",
-  //       "\u0032\u20E3",
-  //       "\u0033\u20E3",
-  //       "\u0034\u20E3",
-  //       "\u0035\u20E3"
-  //     ];
-  //     for (let i = 0; i < arrTracks.length; i++) await msg.react(reactions[i]);
-  //     msg
-  //       .awaitReactions(
-  //         (filter = (reaction, user) =>
-  //           reactions.includes(reaction.emoji.name) &&
-  //           user.id != msg.author.id),
-  //         { max: 1, time: 10000 }
-  //       )
-  //       .then(async collected => {
-  //         if (!collected.first()) return;
-  //         const reaction = collected.first();
-  //         const track = arrTracks[reactions.indexOf(reaction.emoji.name)];
-  //         const res = await axios.get(
-  //           `${root}/track.lyrics.get?apikey=${token}&commontrack_id=${track.ID}`
-  //         );
-  //         delete arrTracks;
-  //         if (res.data.message.header.status_code != 200) {
-  //           message.channel.send(
-  //             `Lyrics not Found. Error ${res.data.message.header.status_code}.`
-  //           );
-  //           return;
-  //         }
-  //         message.channel.send(
-  //           `Upcoming Lyrics of ${track.track} by ${track.artist}`
-  //         );
-  //         let lyrics = res.data.message;
-  //         lyrics = lyrics.body.lyrics.lyrics_body;
-  //         lyrics = lyrics.split("**")[0];
-  //         lyrics +=
-  //           "Lyrics powered by www.musixmatch.com. This Lyrics is NOT for Commercial use and only 30% of the lyrics are returned.\n...\n\nBot created by hjjinx#1993. Contact hjjinx@gmail.com";
-  //         // lyricsArr = [lyrics];
-  //         sendLyrics(lyrics);
-  //       })
-  //       .catch(err => console.log(err));
-  //   })
-  //   .catch(err => console.log(err));
 };
 
 const sendLyrics = (message, lyrics) => {
@@ -99,5 +21,5 @@ const sendLyrics = (message, lyrics) => {
     lyricsArr[lyricsArr.length - 1] = lyrics1;
     lyricsArr.push(lyrics2);
   }
-  lyricsArr.forEach(lyrics => message.channel.send(`\`\`\`${lyrics}\`\`\``));
+  lyricsArr.forEach((lyrics) => message.channel.send(`\`\`\`${lyrics}\`\`\``));
 };
--- Music/music.js
+++ Music/music.js
@@ -6,7 +6,231 @@ var song; // Current Playing song message
 
 var dispatchers = {};
 
-module.exports.playSong = message => {
+module.exports.pauseSong = async (message) => {
+  const guildId = message.channel.guild.id;
+  if (song) {
+    song.content = song.content.split(`\`\``);
+    if (!dispatchers[guildId].paused) {
+      await dispatchers[guildId].pause();
+      song.content[3] = "Paused";
+    }
+    song.content = song.content.join(`\`\``);
+    song.edit(song.content);
+    message.delete();
+  }
+};
+
+module.exports.resumeSong = async (message) => {
+  const guildId = message.channel.guild.id;
+  if (song) {
+    song.content = song.content.split(`\`\``);
+    if (dispatchers[guildId].paused) {
+      await dispatchers[guildId].resume();
+      song.content[3] = "Playing";
+    }
+    song.content = song.content.join(`\`\``);
+    song.edit(song.content);
+    message.delete();
+  }
+};
+
+module.exports.changeVolume = (message) => {
+  const guildId = message.channel.guild.id;
+  const content = parseInt(message.content.substr(8));
+  if (Number.isNaN(content) || content > 100 || content < 0) {
+    message.reply(`Volume must be between 0 and 100`);
+    return;
+  }
+  dispatchers[guildId].setVolume(content / 100);
+  if (song.content) {
+    song.content = song.content.split(`\`\``);
+    song.content[1] = `${dispatchers[guildId].volume * 100}%`;
+    song.content = song.content.join(`\`\``);
+    song.edit(song.content);
+  }
+  message.delete();
+};
+
+module.exports.leaveChannel = (message) => {
+  const guildId = message.channel.guild.id;
+  if (message.guild.me.voice.channel && dispatchers[guildId]) {
+    dispatchers[guildId].end();
+    message.guild.me.voice.channel.leave();
+  }
+  message.delete();
+};
+
+module.exports.mute = (message) => {
+  const guildId = message.channel.guild.id;
+  if (song && dispatchers[guildId]) {
+    dispatchers[guildId].setVolume(0);
+    song.content = song.content.split(`\`\``);
+    song.content[3] = "Muted";
+  }
+  song.content = song.content.join(`\`\``);
+  song.edit(song.content);
+  message.delete();
+};
+
+module.exports.stopSong = (message) => {
+  const guildId = message.channel.guild.id;
+  if (message.guild.me.voice.channel) {
+    if (song.content && dispatchers[guildId]) {
+      dispatchers[guildId].setVolume(0);
+      song.content = song.content.split(`\`\``);
+      song.content[3] = "Stopped";
+    }
+    song.content = song.content.join(`\`\``);
+    song.edit(song.content);
+    dispatchers[guildId].end();
+    message.delete();
+  }
+};
+
+module.exports.streamSong = async (message) => {
+  const guildId = message.channel.guild.id;
+  if (message.member.voice.channel) {
+    let url = message.content.substr(8);
+    const ytdl = require("ytdl-core");
+    if (url.startsWith("http")) {
+      await playStream(url, message);
+      return;
+    }
+    // If not a url, then search for the song on YouTube using youtubeApi.js
+    const yt = require("./youtubeApi");
+    let urlArr = await yt.search(url);
+    const embed = new Discord.MessageEmbed()
+      .setColor(0xfd0016)
+      .setTitle("Search Results Found: ");
+    for (let i = 0; i < urlArr.length; i++) {
+      embed.addField(
+        `${i + 1}: ${urlArr[i].title}`,
+        `https://youtube.com${urlArr[i].href}`
+      );
+    }
+    message.channel.send({ embed }).then(async (msg) => {
+      const reactions = [
+        "\u0031\u20E3",
+        "\u0032\u20E3",
+        "\u0033\u20E3",
+        "\u0034\u20E3",
+        "\u0035\u20E3",
+      ];
+      msg
+        .awaitReactions(
+          (reaction, user) =>
+            reactions.includes(reaction.emoji.name) && user.id != msg.author.id,
+          { max: 1, time: 20000 }
+        )
+        .then(async (collected) => {
+          if (!collected.first()) return;
+          const reaction = collected.first();
+          url = urlArr[reactions.indexOf(reaction.emoji.name)];
+          playStream(url.href, message);
+        });
+
+      for (let i = 0; i < urlArr.length; i++) {
+        setTimeout(() => msg.react(reactions[i]), 10 * i);
+        // await msg.react(reactions[i]);
+      }
+    });
+  } else message.reply(`You must join a voice channel first!!`);
+};
+
+playStream = async (url, message) => {
+  const guildId = message.channel.guild.id;
+  const ytdl = require("ytdl-core");
+  const songDetails = await ytdl.getBasicInfo(url);
+  song = await message.channel.send(
+    `Playing :musical_note: ${songDetails.title} :musical_note: 
+Volume: \`\` 20% \`\`
+Status: \`\` Playing \`\``
+  );
+  const reactions = [`⏯`, `🔈`, `🔊`];
+  for (reaction of reactions) await song.react(reaction);
+  // Member will always be in a voice channel at this point.
+  message.member.voice.channel
+    .join()
+    .then(async (connection) => {
+      dispatchers[guildId] = "";
+      let stream = ytdl(url, {
+        filter: "audioonly",
+        quality: "highestaudio",
+        liveChunkReadahead: 10,
+        highWaterMark: 1 << 20,
+      });
+      dispatchers[guildId] = connection.play(stream);
+
+      stream.on("error", console.log);
+      // stream.on("data", console.log);
+      // stream.on("progress", console.log);
+      stream.on("end", () => console.log(`End of stream`));
+
+      dispatchers[guildId].setVolume(0.2);
+      dispatchers[guildId].on("finish", (end) => {
+        song.content = song.content.split(`\`\``);
+        song.content[3] = "Ended";
+        song.content = song.content.join(`\`\``);
+        song.edit(song.content);
+        message.guild.me.voice.channel.leave();
+        collector.stop();
+      });
+    })
+    .catch(console.error);
+
+  collector = song.createReactionCollector(
+    // Filter for collecting reactions. Only reactions passing through filter collected
+    (reaction, user) =>
+      reactions.includes(reaction.emoji.name) && user.id != song.author.id,
+    { time: 1000000 } // Long time
+  );
+  collector.on("collect", async (reaction, reactionCollector) => {
+    const index = reactions.indexOf(reaction.emoji.name);
+    if (index === 0) {
+      // if (song.reactions.get(reactions[0]).count % 2 === 0)
+      //   dispatcher.pause();
+      // else dispatcher.play();
+      song.content = song.content.split(`\`\``);
+      if (dispatchers[guildId].paused) {
+        await dispatchers[guildId].resume();
+        song.content[3] = "Playing";
+      } else {
+        await dispatchers[guildId].pause();
+        song.content[3] = "Paused";
+      }
+      song.content = song.content.join(`\`\``);
+      song.edit(song.content);
+    } else if (index === 1) {
+      // Decrease Volume
+      if (dispatchers[guildId].volume && dispatchers[guildId].volume > 0.1) {
+        await dispatchers[guildId].setVolume(
+          Math.round((dispatchers[guildId].volume - 0.1) * 10) / 10
+        );
+      } else await dispatchers[guildId].setVolume(0);
+      song.content = song.content.split(`\`\``);
+      song.content[1] = `${dispatchers[guildId].volume * 100}%`;
+      song.content = song.content.join(`\`\``);
+      song.edit(song.content);
+    } else if (index === 2) {
+      // Increase Volume
+      if (dispatchers[guildId].volume && dispatchers[guildId].volume < 0.9) {
+        await dispatchers[guildId].setVolume(
+          Math.round((dispatchers[guildId].volume + 0.1) * 10) / 10
+        );
+      } else await dispatchers[guildId].setVolume(1);
+      song.content = song.content.split(`\`\``);
+      song.content[1] = `${dispatchers[guildId].volume * 100}%`;
+      song.content = song.content.join(`\`\``);
+      song.edit(song.content);
+    }
+  });
+  collector.on("end", (collected) => {
+    return;
+  });
+};
+
+// Streams a song directly from the server.
+module.exports.playSong = (message) => {
   const guildId = message.channel.guild.id;
   const rootMusicDir = "D:Music";
   fs.readdir(rootMusicDir, async (err, tempFiles) => {
@@ -81,7 +305,7 @@ Status: \`\` Playing \`\``
             song.edit(song.content);
           }
         });
-        collector.on("end", collected => {
+        collector.on("end", (collected) => {
           return;
         });
         // song.awaitReactions(
@@ -92,11 +316,11 @@ Status: \`\` Playing \`\``
         // ).then(async collected)
         message.member.voice.channel
           .join()
-          .then(async connection => {
+          .then(async (connection) => {
             dispatchers[guildId] = await connection.playFile(
               `${rootMusicDir}${matching[0]}`
             );
-            dispatchers[guildId].on("finish", end => {
+            dispatchers[guildId].on("finish", (end) => {
               song.content = song.content.split(`\`\``);
               song.content[3] = "Ended";
               song.content = song.content.join(`\`\``);
@@ -123,227 +347,5 @@ Status: \`\` Playing \`\``
         `The server has no tracks that match your search query. Please try some other song.`
       );
     }
-  });
-};
-
-module.exports.pauseSong = async message => {
-  const guildId = message.channel.guild.id;
-  if (song) {
-    song.content = song.content.split(`\`\``);
-    if (!dispatchers[guildId].paused) {
-      await dispatchers[guildId].pause();
-      song.content[3] = "Paused";
-    }
-    song.content = song.content.join(`\`\``);
-    song.edit(song.content);
-    message.delete();
-  }
-};
-
-module.exports.resumeSong = async message => {
-  const guildId = message.channel.guild.id;
-  if (song) {
-    song.content = song.content.split(`\`\``);
-    if (dispatchers[guildId].paused) {
-      await dispatchers[guildId].resume();
-      song.content[3] = "Playing";
-    }
-    song.content = song.content.join(`\`\``);
-    song.edit(song.content);
-    message.delete();
-  }
-};
-
-module.exports.changeVolume = message => {
-  const guildId = message.channel.guild.id;
-  const content = parseInt(message.content.substr(8));
-  if (Number.isNaN(content) || content > 100 || content < 0) {
-    message.reply(`Volume must be between 0 and 100`);
-    return;
-  }
-  dispatchers[guildId].setVolume(content / 100);
-  if (song.content) {
-    song.content = song.content.split(`\`\``);
-    song.content[1] = `${dispatchers[guildId].volume * 100}%`;
-    song.content = song.content.join(`\`\``);
-    song.edit(song.content);
-  }
-  message.delete();
-};
-
-module.exports.leaveChannel = message => {
-  const guildId = message.channel.guild.id;
-  if (message.guild.me.voice.channel && dispatchers[guildId]) {
-    dispatchers[guildId].end();
-    message.guild.me.voice.channel.leave();
-  }
-  message.delete();
-};
-
-module.exports.mute = message => {
-  const guildId = message.channel.guild.id;
-  if (song && dispatchers[guildId]) {
-    dispatchers[guildId].setVolume(0);
-    song.content = song.content.split(`\`\``);
-    song.content[3] = "Muted";
-  }
-  song.content = song.content.join(`\`\``);
-  song.edit(song.content);
-  message.delete();
-};
-
-module.exports.stopSong = message => {
-  const guildId = message.channel.guild.id;
-  if (message.guild.me.voice.channel) {
-    if (song.content && dispatchers[guildId]) {
-      dispatchers[guildId].setVolume(0);
-      song.content = song.content.split(`\`\``);
-      song.content[3] = "Stopped";
-    }
-    song.content = song.content.join(`\`\``);
-    song.edit(song.content);
-    dispatchers[guildId].end();
-    message.delete();
-  }
-};
-
-module.exports.streamSong = async message => {
-  const guildId = message.channel.guild.id;
-  if (message.member.voice.channel) {
-    let url = message.content.substr(8);
-    const ytdl = require("ytdl-core");
-    if (url.startsWith("http")) {
-      await playStream(url, message);
-      return;
-    }
-    // If not a url, then search for the song on YouTube using youtubeApi.js
-    const yt = require("./youtubeApi");
-    let urlArr = await yt.search(url);
-    const embed = new Discord.MessageEmbed()
-      .setColor(0xff00aa)
-      .setTitle("Search Results Found: ");
-    for (let i = 0; i < urlArr.length; i++) {
-      embed.addField(
-        `${i + 1}: ${urlArr[i].title}`,
-        `https://youtube.com${urlArr[i].href}`
-      );
-    }
-    message;
-    message.channel.send({ embed }).then(async msg => {
-      const reactions = [
-        "\u0031\u20E3",
-        "\u0032\u20E3",
-        "\u0033\u20E3",
-        "\u0034\u20E3",
-        "\u0035\u20E3"
-      ];
-      msg
-        .awaitReactions(
-          (filter = (reaction, user) =>
-            reactions.includes(reaction.emoji.name) &&
-            user.id != msg.author.id),
-          { max: 1, time: 20000 }
-        )
-        .then(async collected => {
-          if (!collected.first()) return;
-          const reaction = collected.first();
-          url = urlArr[reactions.indexOf(reaction.emoji.name)];
-          playStream(url.href, message);
-        });
-
-      for (let i = 0; i < urlArr.length; i++) await msg.react(reactions[i]);
-    });
-  } else message.reply(`You must join a voice channel first!!`);
-};
-
-playStream = async (url, message) => {
-  const guildId = message.channel.guild.id;
-  const ytdl = require("ytdl-core");
-  const songDetails = await ytdl.getBasicInfo(url);
-  song = await message.channel.send(
-    `Playing :musical_note: ${songDetails.title} :musical_note: 
-Volume: \`\` 20% \`\`
-Status: \`\` Playing \`\``
-  );
-  const reactions = [`⏯`, `🔈`, `🔊`];
-  for (reaction of reactions) await song.react(reaction);
-  // Member will always be in a voice channel at this point.
-  message.member.voice.channel
-    .join()
-    .then(async connection => {
-      dispatchers[guildId] = "";
-      let stream = ytdl(url, {
-        filter: "audioonly",
-        quality: "highestaudio",
-        liveChunkReadahead: 10,
-        highWaterMark: 1 << 20
-      });
-      dispatchers[guildId] = connection.play(stream);
-
-      stream.on("error", console.log);
-      // stream.on("data", console.log);
-      // stream.on("progress", console.log);
-      stream.on("end", () => console.log(`End of stream`));
-
-      dispatchers[guildId].setVolume(0.2);
-      dispatchers[guildId].on("finish", end => {
-        song.content = song.content.split(`\`\``);
-        song.content[3] = "Ended";
-        song.content = song.content.join(`\`\``);
-        song.edit(song.content);
-        message.guild.me.voice.channel.leave();
-        collector.stop();
-      });
-    })
-    .catch(console.error);
-
-  collector = song.createReactionCollector(
-    // Filter for collecting reactions. Only reactions passing through filter collected
-    (reaction, user) =>
-      reactions.includes(reaction.emoji.name) && user.id != song.author.id,
-    { time: 1000000 } // Long time
-  );
-  collector.on("collect", async (reaction, reactionCollector) => {
-    const index = reactions.indexOf(reaction.emoji.name);
-    if (index === 0) {
-      // if (song.reactions.get(reactions[0]).count % 2 === 0)
-      //   dispatcher.pause();
-      // else dispatcher.play();
-      song.content = song.content.split(`\`\``);
-      if (dispatchers[guildId].paused) {
-        await dispatchers[guildId].resume();
-        song.content[3] = "Playing";
-      } else {
-        await dispatchers[guildId].pause();
-        song.content[3] = "Paused";
-      }
-      song.content = song.content.join(`\`\``);
-      song.edit(song.content);
-    } else if (index === 1) {
-      // Decrease Volume
-      if (dispatchers[guildId].volume && dispatchers[guildId].volume > 0.1) {
-        await dispatchers[guildId].setVolume(
-          Math.round((dispatchers[guildId].volume - 0.1) * 10) / 10
-        );
-      } else await dispatchers[guildId].setVolume(0);
-      song.content = song.content.split(`\`\``);
-      song.content[1] = `${dispatchers[guildId].volume * 100}%`;
-      song.content = song.content.join(`\`\``);
-      song.edit(song.content);
-    } else if (index === 2) {
-      // Increase Volume
-      if (dispatchers[guildId].volume && dispatchers[guildId].volume < 0.9) {
-        await dispatchers[guildId].setVolume(
-          Math.round((dispatchers[guildId].volume + 0.1) * 10) / 10
-        );
-      } else await dispatchers[guildId].setVolume(1);
-      song.content = song.content.split(`\`\``);
-      song.content[1] = `${dispatchers[guildId].volume * 100}%`;
-      song.content = song.content.join(`\`\``);
-      song.edit(song.content);
-    }
-  });
-  collector.on("end", collected => {
-    return;
   });
 };
